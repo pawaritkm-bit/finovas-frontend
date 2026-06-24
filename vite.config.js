ebc1da7067f4fc1b9bd386bbb5ef19c8b73b7945
pawaritkm-bit <pawaritk.m@gmail.com>
2026-06-24 15:36:29 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,13 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: { port: 5173 },
-  build:  { outDir: 'dist' },
+  build: { 
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        liff: 'liff-form.html'
+      }
+    }
+  },
 });
